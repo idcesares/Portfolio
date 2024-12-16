66280bb8eafa0a6532cde8e68c56c408b891eb79
Isaac D'Césares <isaac.dcesares@gmail.com>
2024-12-16 17:39:48 -0300

    Site Upgrade Astro v5
    Multiple compatibility fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 import sitemap from "@astrojs/sitemap";
 import partytown from '@astrojs/partytown';
 import mdx from '@astrojs/mdx';
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,9 @@
+import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
-import { rssSchema } from '@astrojs/rss';
 
 export const collections = {
 	work: defineCollection({
-		type: 'content',
+		loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/work" }),
 		schema: z.object({
 			title: z.string(),
 			description: z.string(),
@@ -14,7 +14,7 @@ export const collections = {
 		}),
 	}),
 	blog: defineCollection({
-		type: 'content',
+		loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/blog" }),
 		schema: z.object({
 			title: z.string(),
 			description: z.string(),
