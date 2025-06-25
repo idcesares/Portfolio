4e7e18d0301eb36c5c0f7ff75f590877c1dbd694
Isaac D'Césares <isaac.dcesares@gmail.com>
2025-06-25 11:02:09 -0300

    Upgrade Tailwind & others

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,29 +5,39 @@ import partytown from '@astrojs/partytown';
 import mdx from '@astrojs/mdx';
 import embeds from 'astro-embed/integration';
 import react from "@astrojs/react";
-import tailwind from '@astrojs/tailwind';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   adapter: vercel({
     webAnalytics: {
       enabled: true
     }
   }),
+
   build: {
     format: "file",
   },
+
   site: 'https://dcesares.dev',
+
   integrations: [sitemap(), partytown({
     config: {
       forward: ["dataLayer.push"]
     }
-  }), embeds(), mdx(), react(), tailwind()],
+  }), embeds(), mdx(), react()],
+
   markdown: {
     shikiConfig: {
       theme: 'dracula',
       wrap: true,
     },
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
   }
 });
